a5bb5a6ae901c15529e218499df11f54d1e91bc6
KacperLukowiak <66062821+KacperLukowiak@users.noreply.github.com>
2020-05-28 18:21:30 +0200

    Add files via upload

---
+++ Models/CustomerModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomersLibrary.Models
+{
+    public class CustomerModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string StreetName { get; set; }
+        public int HouseNumber { get; set; }
+        public string ApartmentNumber { get; set; }
+        public string PostalCode { get; set; }
+        public string Town { get; set; }
+        public int PhoneNumber { get; set; }
+        public string DateOfBirth { get; set; }
+        public int Age { get; set; }
+    }
+}
---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,66 @@
+﻿using Caliburn.Micro;
+using CustomersLibrary.Models;
+using System;
+using System.IO;
+using System.Linq;
+using System.Xml.Serialization;
+
+namespace Customers.ViewModels
+{
+    public class MainViewModel : PropertyChangedBase
+    {
+        private BindableCollection<CustomerModel> _customers = new BindableCollection<CustomerModel>();
+        public BindableCollection<CustomerModel> Customers
+        {
+            get
+            {
+                return _customers;
+            }
+            set
+            {
+                _customers = value;
+                NotifyOfPropertyChange(() => Customers);
+            }
+        }
+        readonly XmlSerializer serializer = new XmlSerializer(typeof(BindableCollection<CustomerModel>));
+        
+        public MainViewModel()
+        {
+            ReadDataXML();
+        }
+
+
+        public void SaveToXML()
+        {
+            var userPath = @"%USERPROFILE%\CustomerData.xml";
+            var filePath = Environment.ExpandEnvironmentVariables(userPath);
+            File.Delete(filePath);
+            using (FileStream stream = File.OpenWrite(filePath))
+            {
+                serializer.Serialize(stream, Customers);
+            }
+        }
+
+
+        public void CancelData()
+        {
+            Customers.Clear();
+            ReadDataXML();
+        }
+
+
+        public void ReadDataXML()
+        {
+            var userPath = @"%USERPROFILE%\CustomerData.xml";
+            var filePath = Environment.ExpandEnvironmentVariables(userPath);
+            if (File.Exists(filePath))
+            {
+                using (FileStream streamRead = File.OpenRead(filePath))
+                {
+                    Customers = (BindableCollection<CustomerModel>)serializer.Deserialize(streamRead);
+                }
+            }
+        }
+
+    }
+}
